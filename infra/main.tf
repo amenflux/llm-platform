bc3a03c194e46d1df5eb364710a906213b5a364e
Amen Bouteraa <amen@bouteraa.com>
2026-07-24 14:43:40 +0200

    feat(infra): added compartment and remote state backend (#5)

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,23 @@ data "oci_identity_availability_domains" "ads" {
   compartment_id = var.tenancy_ocid
 }
 
-output "availability_domains" {
-  value = data.oci_identity_availability_domains.ads.availability_domains
+resource "oci_identity_compartment" "llm_platform" {
+  compartment_id = var.tenancy_ocid   # parent = the root/tenancy
+  name           = "llm-platform"
+  description    = "Compartment for the LLM platform project"
+  enable_delete  = true               # lets `terraform destroy` remove it later
+}
+
+output "compartment_id" {
+  value = oci_identity_compartment.llm_platform.id
+}
+
+data "oci_objectstorage_namespace" "ns" {
+  compartment_id = var.tenancy_ocid
+}
+
+resource "oci_objectstorage_bucket" "tf_state" {
+  compartment_id = oci_identity_compartment.llm_platform.id
+  namespace      = data.oci_objectstorage_namespace.ns.namespace
+  name           = "terraform-state"
 }
